6d575bf6e40a2cd3bb6db6fb6f9d9fbdc9f0f737
ghazalasfr <ghazal.lsfr@gmail.com>
2024-01-30 11:22:00 -0800

    left node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - ...
+ *
+ * @parent: ...
+ * @value: ...
+ * Return: ...
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftnode = NULL, *NewNode = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	NewNode = malloc(sizeof(binary_tree_t));
+
+	if (NewNode == NULL)
+		return (NULL);
+
+	NewNode->parent = parent;
+	NewNode->left = NULL;
+	NewNode->right = NULL;
+	NewNode->n = value;
+
+	leftnode = parent->left;
+	parent->left = NewNode;
+
+	if (leftnode)
+	{
+		leftnode->parent = NewNode;
+		NewNode->left = leftnode;
+	}
+
+	return (NewNode);
+}
+
